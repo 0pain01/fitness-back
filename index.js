58c46436e95a560a7d9ec87f0be6c0f3fab41b54
Pankaj singh <pankaj.koranga.098@gmail.com>
2022-12-25 20:54:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,10 @@ const app = express();
 const PORT = 3000;
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
+    res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 
-var cors = require('cors');
-app.use(cors());
 // body parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
